4b8a432417a9dbd5d4c53d5f2b90ab69c19e72f2
Elijah Quesada <this_boy10@yahoo.com>
2018-04-16 10:35:17 -0700

    General JS
    General JS layout created

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
 $(document).ready(function() {
   $.ajax({
-   url: 'https://randomuser.me/api/',
+   url: 'https://randomuser.me/api/?results=12&nat=us&inc=name,picture,email,location,cell,dob',
    dataType: 'json',
    success: function(data) {
-     console.log(data);
-   }
+     $.each(data, function (i, value) {
+
+        $.each(data[i], function(key, value) {
+
+
+          $(".contact-name").each(function() {
+
+
+             $(".contact-name").text(value.name.first.toUpperCase().charAt(0) + value.name.first.slice(1).toLowerCase() +  ' '
+                             + value.name.last.toUpperCase().charAt(0) + value.name.last.slice(1).toLowerCase()); //Capitilizes the first lettter
+
+
+                });
+
+             $(".contact-email").each(function() {
+                $(".contact-email").text(value.email);
+                                });
+
+                                $(".contact-city").each(function() {
+                                   $(this).text(value.location.city);
+
+
+                              });
+
+                              $(".photo").each(function() {
+                                 $(".photo").attr("src", value.picture.large);
+
+
+                            });
+
+
+
+
+           });
  });
-       
+}
+});
 });
