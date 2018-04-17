b31027e0491c08c3175b5d2660606f6d5a3b6426
Elijah Quesada <this_boy10@yahoo.com>
2018-04-17 15:39:54 -0700

    Cleaned up JS

--- js/app.js
+++ js/app.js
@@ -1,42 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function() {  
   $.ajax({
    url: 'https://randomuser.me/api/?results=12&nat=us&inc=name,picture,email,location,cell,dob',
    dataType: 'json',
    success: function(data) {
-     $.each(data, function (i, value) {
+  console.log(data);
 
-        $.each(data[i], function(key, value) {
-
-
-          $(".contact-name").each(function() {
-
-
-             $(".contact-name").text(value.name.first.toUpperCase().charAt(0) + value.name.first.slice(1).toLowerCase() +  ' '
-                             + value.name.last.toUpperCase().charAt(0) + value.name.last.slice(1).toLowerCase()); //Capitilizes the first lettter
-
-
-                });
-
-             $(".contact-email").each(function() {
-                $(".contact-email").text(value.email);
-                                });
-
-                                $(".contact-city").each(function() {
-                                   $(this).text(value.location.city);
-
-
-                              });
-
-                              $(".photo").each(function() {
-                                 $(".photo").attr("src", value.picture.large);
-
-
-                            });
+     $.each(data.results, function (index, value) {
 
 
 
 
-           });
+       $("p.contact-name").text(value.name.first.toUpperCase().charAt(0) + value.name.first.slice(1).toLowerCase() +  ' ' + value.name.last.toUpperCase().charAt(0) + value.name.last.slice(1).toLowerCase());
+
+        $("p.contact-email").text(value.email);
+
+        $(".contact-city").text(value.location.city.toUpperCase().charAt(0) + value.location.city.slice(1).toLowerCase());
+
+        $(".photo").attr("src", value.picture.large);
+
  });
 }
 });
