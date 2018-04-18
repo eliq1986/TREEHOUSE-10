6b2c19e84063f1cde52b9334acfc336868193467
Elijah Quesada <this_boy10@yahoo.com>
2018-04-18 16:33:29 -0700

    Basic Modal Makeup

--- js/app.js
+++ js/app.js
@@ -1,39 +1,53 @@
+const nameContact = document.getElementsByClassName("contact-name");
+const nameEmail = document.getElementsByClassName("contact-email");
+const contactCity = document.getElementsByClassName("contact-city");
+const photoImg = document.getElementsByClassName("photo");
+const modal = document.getElementById("simpleModal");
+const closeBtb = document.querySelector(".closeBtb");
+console.log(closeBtb);
+
 $(document).ready(function() {
   $.ajax({
    url: 'https://randomuser.me/api/?results=12&nat=us&inc=name,picture,email,location,cell,dob',
    dataType: 'json',
    success: function(data) {
-       const nameContact = document.getElementsByClassName("contact-name");
-       const nameEmail = document.getElementsByClassName("contact-email");
-       const contactCity = document.getElementsByClassName("contact-city");
-       const photoImg = document.getElementsByClassName("photo");
-
-
-
 
 
          for (let i =0; i<data.results.length; i++) {
-             nameContact[i].innerHTML = data.results[i].name.first.toUpperCase().charAt(0) + data.results[i].name.first.slice(1).toLowerCase() +  ' ' + data.results[i].name.last.toUpperCase().charAt(0) + data.results[i].name.last.slice(1).toLowerCase();
+
+              let completeName = data.results[i].name.first.toUpperCase().charAt(0) + data.results[i].name.first.slice(1).toLowerCase()
+              +  ' ' + data.results[i].name.last.toUpperCase().charAt(0) + data.results[i].name.last.slice(1).toLowerCase();
+              nameContact[i].innerHTML = completeName;
 }
 
          for (let i =0; i<data.results.length; i++) {
+
              nameEmail[i].innerHTML = data.results[i].email;
 }
 
-
          for (let i =0; i<data.results.length; i++) {
+
              contactCity[i].innerHTML = data.results[i].location.city.toUpperCase().charAt(0) + data.results[i].location.city.slice(1);
 }
+         for (let i =0; i<data.results.length; i++) {
 
-      for (let i =0; i<data.results.length; i++) {
-          photoImg[i].src = data.results[i].picture.large;
+              photoImg[i].src = data.results[i].picture.large;
 
 }
 
+   $(".box").on("click", function(){
+       openModal();
+
+   })
 
 
-
-
+   function openModal() {
+       modal.style.display = "block";
+}
+   function closeModal() {
+      modal.style.display = "none";
+   }
+   closeBtb.addEventListener("click", closeModal);
 
  }
 });
